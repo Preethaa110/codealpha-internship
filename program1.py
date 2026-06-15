03a6b9fc9691dd6f6277cc5e33c00906c8895144
Preethaa110 <25ad110@rmkec.ac.in>
2026-06-15 11:36:48 +0530

    Implement basic chatbot functionality

---
+++ program1.py
@@ -0,0 +1,10 @@
+def chatbot():
+    while True:
+        a= input()
+        if a=="hello":
+            print("Hi")
+        elif a=="how are you":
+            print("I'm fine, thanks!")
+        elif a=="bye":
+            print("Goodbye!")
+chatbot()
